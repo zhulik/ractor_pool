b54fbfea1c54f53416a423bbe22d7377998d187d
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-04-02 21:19:33 +0200

    Initial commit

---
+++ lib/ractor_pool.rb
@@ -0,0 +1,6 @@
+require "ractor_pool/version"
+
+module RactorPool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ractor_pool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RactorPool do
+  it "has a version number" do
+    expect(RactorPool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
